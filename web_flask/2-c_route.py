b9f6050c99aeced393bbd99ff8382a3edc955e5f
tihitna-22 <tihitna222sisay@gmail.com>
2022-04-09 12:29:33 -0700

    add 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This route module 
+starts a Flask web application.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """Prints  hello message"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """Prints  hbnb message"""
+    return 'HBNB'
+
+@app.route('/c/text>')
+def disp_c(text):
+    """Prints  c with text message"""
+    return f"c {text}"
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
